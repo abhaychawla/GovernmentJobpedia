69f6a46705c421dd37822c4acd42a2c8c27d8a27
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-04-08 02:25:54 +0530

    Added splash screen & uncompleted intro-slider

---
+++ app/src/main/java/com/example/android/governmentjobpedia/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.governmentjobpedia;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = findViewById(R.id.splashicon);
+        Handler splash = new Handler();
+        splash.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(splash);
+            }
+        },200);
+    }
+}
